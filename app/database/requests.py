ccfe921a49b87b6f04aec358ce078e8afcb98bb0
kostya-moshchev <kmoshchev00@mail.ru>
2024-06-25 21:57:56 +0300

    Initial commit

---
+++ app/database/requests.py
@@ -0,0 +1,163 @@
+from app.database.models import async_session
+from app.database.models import User, Photo, Location, Winner
+from sqlalchemy import distinct, func, select
+from sqlalchemy import update, delete
+
+
+async def set_user(tg_id, username=None, name=None, second_name=None):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+
+        if not user:
+            session.add(User(tg_id=tg_id, username=username,name=name))
+        else:
+            if name is not None:
+                user.name = name
+            if second_name is not None:
+                user.second_name = second_name
+            if username is not None:
+                user.username = username
+        await session.commit()
+
+async def user_in_bd(tg_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        if user:
+            return True
+        return False
+
+
+async def get_all_locations():
+    async with async_session() as session:
+        result = await session.execute(select(Location))
+        locations = result.scalars().all()
+    return locations
+
+async def get_location(location_id):
+    async with async_session() as session:
+        return await session.scalar(select(Location).where(Location.id == location_id))
+
+async def get_task(location_id):
+    async with async_session() as session:
+        return await session.scalar(select(Location.task).where(Location.id == location_id))
+
+async def get_task_photo(location_id):
+    async with async_session() as session:
+        return await session.scalar(select(Location.task_photo).where(Location.id == location_id))
+
+async def get_descreption_photo(location_id):
+    async with async_session() as session:
+        return await session.scalar(select(Location.descreption_photo).where(Location.id == location_id))
+
+async def save_photo(tg_id,  file_path, location_id, sent_at):
+    await set_user(tg_id)
+    async with async_session() as session:
+        new_photo = Photo(
+            user_tg_id=tg_id,
+            photo=file_path,
+            location_id=location_id,
+            sent_at=sent_at
+        )
+        session.add(new_photo)
+        await session.commit()
+
+async def get_photos_from_db_with_location(id_location, user_id):
+    async with async_session() as session:
+        photo_info = await session.execute(
+        select(Photo.id, Photo.photo).where(
+            Photo.location_id == id_location, Photo.user_tg_id == user_id)
+    )
+    
+    result = photo_info.fetchone()
+    return result
+
+async def get_photo_tg_id(photo_id):
+    async with async_session() as session:
+        return await session.scalar(select(Photo.photo).where(Photo.id == photo_id))
+
+
+# админ
+async def get_photos_from_db(admin_true: int):
+    async with async_session() as session:
+        stmt = (
+            select(Photo, Location.name, User)
+            .join(Location, Photo.location_id == Location.id)
+            .join(User, Photo.user_tg_id == User.tg_id)
+            .where(Photo.admin_true == admin_true)
+            .order_by(Photo.sent_at.asc())
+            .limit(1)
+        )
+        result = await session.execute(stmt)
+        row = result.first()
+        if row:
+            photo, location_name, user = row
+            return {
+                "id": photo.id,
+                "name": user.name,
+                "username": user.username,
+                "photo": photo.photo,
+                "location_name": location_name,
+            }
+        return None
+
+
+async def delete_photo_from_db(photo_id: int):
+    async with async_session() as session:
+        async with session.begin():
+            await session.execute(delete(Photo).where(Photo.id == photo_id))
+
+
+async def update_photo_status(photo_id: int, admin_true: int):
+    async with async_session() as session:
+        async with session.begin():
+            stmt = (
+                update(Photo)
+                .where(Photo.id == photo_id)
+                .values(admin_true=admin_true)
+            )
+            await session.execute(stmt)
+
+
+
+async def check_winner(user_id):
+    async with async_session() as session:
+        async with session.begin():
+            winner_exists = await session.scalar(select(Winner).where(Winner.user_id == user_id))
+            if winner_exists:
+                return False
+            
+            total_locations = await session.scalar(select(func.count(Location.id)))
+            
+            approved_locations_count = await session.scalar(
+                select(func.count(distinct(Photo.location_id)))
+                .where(Photo.user_tg_id == user_id, Photo.admin_true == 1)
+            )
+            
+            if approved_locations_count == total_locations:
+                current_place = await session.scalar(select(func.count(Winner.id))) + 1
+                
+                new_winner = Winner(user_id=user_id, place=current_place)
+                session.add(new_winner)
+                
+                return current_place
+            else:
+                return False
+
+async def answer_admin_win():
+    async with async_session() as session:
+        winners_query = (
+            select(Winner, User.name, User.username)
+            .join(User, Winner.user_id == User.id)
+            .order_by(Winner.place)
+        )
+        
+        winners_rows = await session.execute(winners_query)
+        winners = [
+            f"Место: {winner.place}, Имя: {name}, Username: @{username}"
+            for winner, name, username in winners_rows
+        ]
+        return winners
+
+async def get_user_id_by_photo_id(photo_id):
+    async with async_session() as session:
+        return await session.scalar(select(Photo.user_tg_id).where(Photo.id == photo_id))
---
+++ app/keybords.py
@@ -0,0 +1,66 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from app.database.requests import get_all_locations
+
+
+reg = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Регистрация', callback_data='reg')]
+])
+main_one = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Начать игру', callback_data='main')],
+    [InlineKeyboardButton(text='Отреактировать имя и фамилию', callback_data='first_name')],
+])
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Начать игру', callback_data='main')]
+])
+
+to_the_main_page = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='На главную', callback_data='main')]
+])
+replace_or_cancel = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Заменить", callback_data="replace"),
+            InlineKeyboardButton(text="Отмена", callback_data="main"),
+        ]
+    ]
+)
+replace_or_cancel_2 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Заменить", callback_data="replace_2"),
+            InlineKeyboardButton(text="Отмена", callback_data="main"),
+        ]
+    ]
+)
+
+async def inline_location():
+    keybord = InlineKeyboardBuilder()
+    locations = await get_all_locations()
+    for location in locations:
+        keybord.add(InlineKeyboardButton(text=location.name, callback_data=f'location_{location.id}'))
+    return keybord.adjust(1).as_markup()
+
+def location_info(location):
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Маршрут', url=location.locations)],
+        [InlineKeyboardButton(text='Задание', callback_data=f'task_{location.id}')],
+        [InlineKeyboardButton(text='Отправить фото', callback_data=f'my_photo_{location.id}')],
+        [InlineKeyboardButton(text='Назад', callback_data='main')],
+    ])
+
+# админ
+main_admin = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Просмотр фото')],
+    [KeyboardButton(text='🏆Победители🏆')]
+], resize_keyboard=True)
+
+async def create_admin_answer_keyboard(id_photo):
+    admin_answer = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='✅', callback_data=f'yes_{id_photo}'), InlineKeyboardButton(text='❌', callback_data=f'no_{id_photo}')],
+        [InlineKeyboardButton(text='Закрыть', callback_data='main')]
+    ])
+    return admin_answer
